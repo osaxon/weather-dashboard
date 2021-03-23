b0c9dffac69349dbd4e74b26e954494ac7d2c966
Oli <oliverrsaxon@gmail.com>
2021-03-23 21:40:49 +0000

    FEAT: function to get a list of cities

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 let $city = $('.city');
+let $search = $('.search');
+let $searchBtn = $search.children().eq(1);
 
 const API_KEY = 'dda4fd5a4e43b1b488ab9b9eda5872c0'
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=dda4fd5a4e43b1b488ab9b9eda5872c0')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data.name);
-    $city.children().eq(0).text(data.name)
+function getCities (lat, lon, cnt) {
+    fetch('http://api.openweathermap.org/data/2.5/find?lat=' + lat + '&lon=' + lon + '&cnt=' + cnt + '&appid=' + API_KEY)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data.list);
+    });
+}
 
-  });
+
+getCities(53.74, -2.24, 10);
