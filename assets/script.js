ffdd1b922b17b97ca9a864c43e254c6876f4133f
Oli <oliverrsaxon@gmail.com>
2021-03-26 19:42:45 +0000

    FEAT: Saved searches can be passed through to fetch function

--- assets/script.js
+++ assets/script.js
@@ -21,21 +21,33 @@ function getCities (lat, lon, cnt) {
 
 //getCities(53.74, -2.24, 10);
 
-
+// get current weather conditions and display data to main card
 function renderWeather(cityName) {
     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + API_KEY)
     .then(function(response) {
-        return response.json();
+        if(response.ok){
+            return response.json();
+        } else {
+            throw Error("Rejected with status: " + response.status)
+        };
     })
     .then(function(data) {
         let city = data.name;
+        let weather = data.weather;
+        let icon = weather[0].icon;
+        let $iconEl = $('<img>');
+        $iconEl.attr("src", "https://openweathermap.org/img/w/" + icon + ".png")
+        $city.children().eq(0).append($iconEl)
         $city.children().eq(0).text(city)
         $city.children().eq(1).text("Temp: " + data.main.temp + " degrees celcius")
         $city.children().eq(2).text("Humidy: " + data.main.humidity + "%")
         saveSearch(city)
+        renderHistory();
     })
+    .catch(console.error)
 }
 
+// save the search history
 function saveSearch(cityName) {
     console.log(prevSearches.indexOf(cityName))
     if(prevSearches.indexOf(cityName) === -1){
@@ -47,11 +59,23 @@ function saveSearch(cityName) {
     }
 }
 
+// display previous searches
 function renderHistory(){
+    $searchHist.empty();
     prevSearches.forEach(function(element){
         let newLi = $('<li>');
+        newLi.addClass("list-group-item")
         newLi.text(element);
         newLi.appendTo($searchHist);
+    })
+}
+
+function getForecast(cityName) {
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + API_KEY)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
     })
 }
 
@@ -59,6 +83,12 @@ renderHistory();
 
 $searchBtn.on("click", function(){
     renderWeather($search.children().eq(0).val());
+    getForecast($search.children().eq(0).val());
+})
+
+// search via search history
+$searchHist.on("click", function(event){
+    renderWeather(event.target.textContent)
 })
 
 
